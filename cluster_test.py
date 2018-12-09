d71984f62f99045965a74a440fff4c7351c12a7f
Wesley Shann <sshann95@outlook.com>
2018-12-09 16:39:07 +0100

    Copy stopwords to static variable

--- cluster_test.py
+++ cluster_test.py
@@ -90,14 +90,15 @@ def get_NLPsupport(record):
 def tokenizer(record):
     import nltk
     from nltk.tag import StanfordNERTagger
-    from nltk.corpus import stopwords 
+    #from nltk.corpus import stopwords 
     from nltk.tokenize import word_tokenize 
     import sys
     import unicodedata
 
     nltk.data.path.append(os.environ.get('PWD'))
     key,text = record
-    stop_words = set(stopwords.words('english')) 
+    #stop_words = set(stopwords.words('english')) 
+    stop_words = {'its', 'against', 'what', 'so', 'll', "it's", 'where', "wasn't", 'about', 'didn', 'a', 'no', 'we', 'y', 'couldn', "haven't", 'is', 'them', 'be', 'should', 'mustn', "mightn't", 'between', "doesn't", 'her', 'who', 'why', 'd', 've', 'as', 'myself', "didn't", 'having', 'shan', "won't", 'until', 'and', 'itself', 'but', 'had', 'by', 'before', 'each', 'not', 'hadn', "shan't", 'above', 'yours', 'yourself', 'they', 'other', 'don', 'then', "you'd", 'down', 'your', 'such', 'weren', 'ours', 't', 'wouldn', 'does', 'needn', 'the', 'on', "isn't", 'our', 'after', 'most', 'themselves', 'through', 'again', 'there', 'both', 'in', 'shouldn', 'with', 'being', 'those', "you'll", 'ain', 'than', 'he', 'do', 'hasn', 'me', 'if', 'nor', 'aren', 'wasn', 'all', 'can', 'ma', 'you', "wouldn't", 're', 'because', "hasn't", "should've", 'my', 'will', 'from', 'under', 'own', 'to', 'him', 'did', 'here', 'same', 'when', 's', 'or', 'doing', 'o', "mustn't", 'she', "hadn't", 'few', "you're", 'these', "you've", 'while', 'am', 'i', 'too', "aren't", 'up', 'herself', 'now', 'that', 'are', 'very', 'at', 'only', 'm', 'out', "weren't", 'off', 'during', 'his', 'further', 'just', 'of', 'has', 'doesn', 'theirs', 'which', 'an', 'whom', 'more', 'isn', 'mightn', "couldn't", 'for', 'was', 'below', 'any', 'their', 'won', "needn't", 'yourselves', 'over', 'some', 'haven', 'been', 'once', 'into', 'how', 'this', 'himself', 'have', "don't", "shouldn't", 'hers', 'it', "that'll", 'ourselves', "she's", 'were'}
 
     tbl = dict.fromkeys(i for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P')) #Remove pontuactions from text
     text_no_pontuation = text.translate(tbl)
